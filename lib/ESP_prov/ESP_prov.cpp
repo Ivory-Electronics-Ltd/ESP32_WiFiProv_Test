c1fb4597b3e06814b234827c31a3fb09882d3a47
TOOATK <khaotoo13@gmail.com>
2025-10-16 17:01:34 +0700

    Initial Commit

---
+++ lib/ESP_prov/ESP_prov.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file ESP32Provisioning.cpp
+ * @brief ESP32 WiFi Provisioning Library Implementation
+ */
+
+#include "ESP_prov.h"
+#include "config.h"
+
+// Static instance pointer for event handler
---
+++ lib/ESP_prov/ESP_prov.h
@@ -0,0 +1,14 @@
+/**
+ * @file ESP32prov.h
+ * @brief ESP32 WiFi Provisioning Library Header
+ */
+
+#ifndef ESP_PROV_H
+#define ESP_PROV_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiProv.h>
+
+// Event callback function pointer type
+#endif // ESP32_PROVISIONING_H
